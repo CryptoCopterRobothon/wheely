903ec4bc831a2b7ee004e284b8a93cc40bf9849a
Samantha Fuchs <samantha@telering.at>
2016-04-01 19:59:36 +0200

    followLine()

--- wheely/LineFollower.ino
+++ wheely/LineFollower.ino
@@ -1,1 +1,24 @@
+void followLine(){
+  int sensorState = lineFinder.readSensors();
+  switch(sensorState)
+  {
+    case S1_IN_S2_IN: 
+      Serial.println("Sensor 1 and 2 are inside of black line"); 
+      
+      break;
+    case S1_IN_S2_OUT: 
+      Serial.println("Sensor 2 is outside of black line"); 
+      
+      break;
+    case S1_OUT_S2_IN: 
+      Serial.println("Sensor 1 is outside of black line"); 
+      
+      break;
+    case S1_OUT_S2_OUT: 
+      Serial.println("Sensor 1 and 2 are outside of black line");
+      
+      break;
+    default: break;
+  }
+}
 
