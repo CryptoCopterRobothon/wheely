dbef0f63a974665b2a6441d8d886027d9689722d
LucasUllrich <lucas.ullrich@aon.at>
2016-04-01 20:10:28 +0200

    Motors

--- wheely/Motoren.ino
+++ wheely/Motoren.ino
@@ -1,1 +1,13 @@
+void Motors(unsinged char sens_line) {
+  switch(sens_line)
+  {
+    case S1_IN_S2_IN:
+      Serial.println("Stop");
+      motor1.run(20);
+      break;
+    case S1_IN_S2_OUT: Serial.println("Sensor 2 is outside of black line"); break;
+    case S1_OUT_S2_IN: Serial.println("Sensor 1 is outside of black line"); break;
+    case S1_OUT_S2_OUT: Serial.println("Sensor 1 and 2 are outside of black line"); break;
+    default: break;
+}
 
--- wheely/wheely.ino
+++ wheely/wheely.ino
@@ -1,11 +1,14 @@
 #include "MeOrion.h"
 
+MeDCMotor motor1(M1);
+MeLineFollower lineFinder(PORT_3);
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Motors(lineFinder.readSensors());
 }
