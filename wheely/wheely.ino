2c6fdc37e84d16ac333034438efddfe75e59de5d
Chrisini <christina.bornberg@outlook.com>
2016-04-02 04:15:59 +0200

    whatever

--- wheely/wheely.ino
+++ wheely/wheely.ino
@@ -2,8 +2,6 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
-//#define SPIN
-
 /*
  * 0: innerhalb
  * 1: rechts
@@ -11,16 +9,12 @@
  * 3: außerhalb
  */
 uint8_t lineDirection = 0;
-uint8_t motor_default = 40;
+uint8_t motor_default = 20;
 uint8_t left = 0;
 uint8_t right = 0;
 uint8_t checkpointCounter = 0;
 uint8_t endpoint = 0;
 bool insideCheckpointLine = false;
-bool crosswise = false;
-double standardAngle;
-bool isSpin = false;
-
 //Remote
 uint8_t ReceiverCode;
 uint8_t buttonState;
@@ -31,12 +25,11 @@ int armSpeed = 250;
 int nipSpeed = 250;
 int handSpeed = 250;
 
-MeDCMotor motor_right(M1);
-MeDCMotor motor_left(M2);
+MeDCMotor motor_right(PORT1);
+MeDCMotor motor_left(PORT2);
 MeLineFollower lineFinder(PORT_3);
 MeLineFollower lineCounter(PORT_4);
 MeInfraredReceiver infraredReceiverDecode(PORT_6);
-MeCompass Compass(PORT_7);
 // grappler
 // Arm = Arm (oben unten)
 MeDCMotor arm(M1);
@@ -49,27 +42,17 @@ MeDCMotor hand(PORT_5);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  infraredReceiverDecode.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   if(checkpointCounter != endpoint){
-   // Serial.println("testprint");
-    if(!crosswise){
-      followLine();
-      receive();
-    }else{
-      if(checkpointCounter < endpoint){
-        spin(0,1);
-      }else{
-        spin(1,1);
-      }
-    }
-
+    followLine();
+    receive();
   }else{
-    if(isSpin){
-      spin(1,1);
+    if(spin){
+      Motors(0,0);
     }else{
       receive();
     }
